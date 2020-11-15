763ba2bd8739ed60f107945f02d6488800cb7074
Hans DeJong <72646065+hansbdejong@users.noreply.github.com>
2020-11-15 20:24:23 +0900

    Update Delta_omega_RS_hydrocasts.m

--- code/Delta_omega_RS_hydrocasts.m
+++ code/Delta_omega_RS_hydrocasts.m
@@ -18,13 +18,13 @@ TA_200_400 = A(:,18);
 sal_200_400 = A(:,16);
 
 output = CO2SYS(TA,DIC,1,2,sal,temp,temp,0,0,0,0,1,4,1);
-Omega_surface=output(:,31);
+Omega_surface = output(:,31);
 
 %set temp to -1.89 C
-output_winter=CO2SYS(TA_200_400,DIC_200_400,1,2,sal_200_400,-1.89,-1.89,0,0,0,0,1,4,1);
+output_winter = CO2SYS(TA_200_400,DIC_200_400,1,2,sal_200_400,-1.89,-1.89,0,0,0,0,1,4,1);
 Omega_winter = output_winter(:,31);
 
-final=horzcat(Omega_surface, Omega_winter);
-filename='Omega_hydrocasts.xlsx'
+final = horzcat(Omega_surface, Omega_winter);
+filename = 'Omega_hydrocasts.xlsx'
 xlswrite(filename,final)
 
